68018e390c7cedfb59810c9c498288c539822443
CatherineBxyz <catherineeB@proton.me>
2024-11-10 19:21:48 +0100

    Add js folder
    Contains script.js file

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const lightbox = document.getElementById("lightbox");
+
+const thumbnails = document.querySelectorAll(".thumbnail");
+thumbnails.forEach(thumb => {
+  thumb.addEventListener("click", (event) => {
+    //const src = thumb.getAttribute("src")?.replace(/^.*\/([^\/]*)$/, "$1");
+    const src = thumb.getAttribute("src")?.replace("small", "big");
+    const largeImage = lightbox.querySelector("img");
+    largeImage.src = src; // Set the large image source
+    lightbox.style.display = "flex"; // Show the lightbox
+  });
+});
+
+lightbox.addEventListener("click", (event) => {
+  lightbox.style.display = "none";
+});
